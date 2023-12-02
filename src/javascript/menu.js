6e22f17c78d383c4d477ecbf2903073149488a2a
Salando 25 <saul374374@gmail.com>
2023-12-02 00:12:16 -0600

    feat: :sparkles: se hizo el menu de hamburguesa

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,7 +1,29 @@
 "use strict"
 
 const menu = document.querySelector("#menu")
+const menu_close = document.querySelector("#menu-close")
+const nav = document.querySelector("#nav")
+const bg = document.querySelector("#bg")
+
+const remove_background = () => {
+  nav.style.translate = "100%"
+  bg.style.height = "0"
+}
+
+const add_background = () => {
+  bg.style.height = "100%"
+  nav.style.translate = "-100%"
+}
+
 
 menu.addEventListener("click", () => {
-  console.log("Hola Mundo")
+  add_background()
+})
+
+menu_close.addEventListener("click", () => {
+  remove_background()
+})
+
+bg.addEventListener("click", () => {
+  remove_background()
 })
